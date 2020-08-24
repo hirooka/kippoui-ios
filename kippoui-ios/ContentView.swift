2f8275589e45e44e2a4df1b855204772592f5c11
kinakoankon <kinakoankon@gmail.com>
2020-08-24 10:06:21 +0900

    Mapでドラッグ、タップ、ロングプレスの併用？

--- kippoui-ios/ContentView.swift
+++ kippoui-ios/ContentView.swift
@@ -13,7 +13,8 @@ struct ContentView: View {
     @State private var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(
             latitude: 35.681236,
-            longitude: 139.767125),
+            longitude: 139.767125
+        ),
         span: MKCoordinateSpan(
             latitudeDelta: 0.01,
             longitudeDelta: 0.01
@@ -21,28 +22,58 @@ struct ContentView: View {
     )
     
     @State private var locations: [Location] = [
-        Location(coordinate: .init(latitude: 35.0, longitude: 139.0)),
-        Location(coordinate: .init(latitude: 36.0, longitude: 140.0)),
-        Location(coordinate: .init(latitude: 37.0, longitude: 141.0))
+        Location(coordinate: .init(latitude: 35.681236, longitude: 139.767125))
     ]
     
     var body: some View {
         VStack {
-            Map(
-                coordinateRegion: $region,
+            Map(coordinateRegion: $region,
                 interactionModes: MapInteractionModes.all,
                 showsUserLocation: true,
-                userTrackingMode: $userTrackingMode
-            )
+                userTrackingMode: $userTrackingMode,
+                annotationItems: locations
+            ) { location in
+                MapPin(coordinate: location.coordinate)
+            }
             .edgesIgnoringSafeArea(.all)
-            .onTapGesture(count: 1, perform: {
+//            .onTapGesture(count: 1, perform: {
+//                withAnimation {
+//                    region.center = CLLocationCoordinate2D(
+//                        latitude: 35.6,
+//                        longitude: 139.7
+//                    )
+//                }
+//            })
+            .highPriorityGesture(
+                DragGesture().onEnded{ value in
+                    
+                }.onChanged(){value in
+                    
+                })
+            .onLongPressGesture {
                 withAnimation {
-                    region.center = CLLocationCoordinate2D(
-                        latitude: 35.6,
-                        longitude: 139.7
-                    )
+                    locations.append(Location(coordinate: .init(latitude: 25.0, longitude: 139.0)))
+                    print("onLongPressGesture")
                 }
-            })
+            }
+//            .gesture(
+//                LongPressGesture().onEnded{ value in
+//                    print("LongPressGesture().onEnded")
+//                }.onChanged(){ value in
+//                }
+//            )
+            .gesture(
+                DragGesture(minimumDistance: 0).onEnded{ value in
+                    print(value.startLocation)
+                }.onChanged(){value in
+                    print(value.startLocation)
+                }
+            )
+            .gesture(
+                TapGesture().onEnded{ value in
+                    print("TapGesture().onEnded")
+                }
+            )
         }
     }
 }

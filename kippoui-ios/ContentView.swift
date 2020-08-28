7b7a2fc1de316ffbccddf9fbd1968757e51596ee
kinakoankon <kinakoankon@gmail.com>
2020-08-28 15:56:00 +0900

    線を引く

--- kippoui-ios/ContentView.swift
+++ kippoui-ios/ContentView.swift
@@ -1,79 +1,40 @@
 import SwiftUI
 import MapKit
 
-struct Location: Identifiable {
-    let id = UUID()
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
     
-    @State private var userTrackingMode: MapUserTrackingMode = .follow
-    
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 35.681236,
-            longitude: 139.767125
-        ),
-        span: MKCoordinateSpan(
-            latitudeDelta: 0.01,
-            longitudeDelta: 0.01
-        )
-    )
-    
-    @State private var locations: [Location] = [
-        Location(coordinate: .init(latitude: 35.681236, longitude: 139.767125))
-    ]
+    @State var count =  0
+    @State var coordinate = CLLocationCoordinate2D()
+    @State var checkPoints: [CheckPoint] = []
+    @State var previousCheckPoints: [CheckPoint] = []
+    @State var coordinates: [CLLocationCoordinate2D] = []
+    @State var coordinates0: [CLLocationCoordinate2D] = []
+    @State var coordinates1: [CLLocationCoordinate2D] = []
+    @State var coordinates2: [CLLocationCoordinate2D] = []
+    @State var coordinates3: [CLLocationCoordinate2D] = []
+    @State var coordinates4: [CLLocationCoordinate2D] = []
+    @State var coordinates5: [CLLocationCoordinate2D] = []
+    @State var coordinates6: [CLLocationCoordinate2D] = []
+    @State var coordinates7: [CLLocationCoordinate2D] = []
     
     var body: some View {
         VStack {
-            Map(coordinateRegion: $region,
-                interactionModes: MapInteractionModes.all,
-                showsUserLocation: true,
-                userTrackingMode: $userTrackingMode,
-                annotationItems: locations
-            ) { location in
-                MapPin(coordinate: location.coordinate)
-            }
+            MapView(
+                count: $count,
+                coordinate: $coordinate,
+                checkPoints: $checkPoints,
+                previousCheckPoints: $previousCheckPoints,
+                coordinates: $coordinates,
+                coordinates0: $coordinates0,
+                coordinates1: $coordinates1,
+                coordinates2: $coordinates2,
+                coordinates3: $coordinates3,
+                coordinates4: $coordinates4,
+                coordinates5: $coordinates5,
+                coordinates6: $coordinates6,
+                coordinates7: $coordinates7
+            )
             .edgesIgnoringSafeArea(.all)
-//            .onTapGesture(count: 1, perform: {
-//                withAnimation {
-//                    region.center = CLLocationCoordinate2D(
-//                        latitude: 35.6,
-//                        longitude: 139.7
-//                    )
-//                }
-//            })
-            .highPriorityGesture(
-                DragGesture().onEnded{ value in
-                    
-                }.onChanged(){value in
-                    
-                })
-            .onLongPressGesture {
-                withAnimation {
-                    locations.append(Location(coordinate: .init(latitude: 25.0, longitude: 139.0)))
-                    print("onLongPressGesture")
-                }
-            }
-//            .gesture(
-//                LongPressGesture().onEnded{ value in
-//                    print("LongPressGesture().onEnded")
-//                }.onChanged(){ value in
-//                }
-//            )
-            .gesture(
-                DragGesture(minimumDistance: 0).onEnded{ value in
-                    print(value.startLocation)
-                }.onChanged(){value in
-                    print(value.startLocation)
-                }
-            )
-            .gesture(
-                TapGesture().onEnded{ value in
-                    print("TapGesture().onEnded")
-                }
-            )
         }
     }
 }
--- kippoui-ios/MapView.swift
+++ kippoui-ios/MapView.swift
@@ -1,16 +1,211 @@
-//import SwiftUI
-//import MapKit
-//
-//struct MapView: UIViewRepresentable {
-//    var location: CLLocationCoordinate2D
-//    
-//    func makeUIView(context: Context) -> MKMapView {
-//        MKMapView(frame: .zero)
-//    }
-//    
-//    func updateUIView(_ uiView: MKMapView, context: Context) {
-//        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-//        let region = MKCoordinateRegion(center: location, span: span)
-//        uiView.setRegion(region, animated: true)
-//    }
-//}
+import SwiftUI
+import MapKit
+
+final class CheckPoint: NSObject, MKAnnotation {
+    let title: String?
+    let coordinate: CLLocationCoordinate2D
+    init(title: String?, coordinate: CLLocationCoordinate2D) {
+        self.title = title
+        self.coordinate = coordinate
+    }
+}
+
+struct MapView: UIViewRepresentable {
+    
+    @Binding var count: Int
+    @Binding var coordinate: CLLocationCoordinate2D
+    @Binding var checkPoints: [CheckPoint]
+    @Binding var previousCheckPoints: [CheckPoint]
+    @Binding var coordinates: [CLLocationCoordinate2D]
+    @Binding var coordinates0: [CLLocationCoordinate2D]
+    @Binding var coordinates1: [CLLocationCoordinate2D]
+    @Binding var coordinates2: [CLLocationCoordinate2D]
+    @Binding var coordinates3: [CLLocationCoordinate2D]
+    @Binding var coordinates4: [CLLocationCoordinate2D]
+    @Binding var coordinates5: [CLLocationCoordinate2D]
+    @Binding var coordinates6: [CLLocationCoordinate2D]
+    @Binding var coordinates7: [CLLocationCoordinate2D]
+    
+    var mapView = MKMapView(frame: .zero)
+    
+    var locationManager = CLLocationManager()
+    func setupManager() {
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+    }
+    
+    func makeUIView(context: Context) -> MKMapView {
+        setupManager()
+        //let mapView = MKMapView(frame: .zero)
+        mapView.delegate = context.coordinator
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        mapView.setRegion(region, animated: true)
+        return mapView
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        //uiView.setCenter(coordinate, animated: true)
+        uiView.removeAnnotations(previousCheckPoints)
+        uiView.addAnnotations(checkPoints)
+        
+        if coordinates.count > 1 && coordinates1.count > 1 {
+            uiView.removeOverlays(mapView.overlays)
+            //let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+            let polyline = MKGeodesicPolyline(coordinates: coordinates, count: coordinates.count)
+            let polyline0 = MKGeodesicPolyline(coordinates: coordinates0, count: coordinates0.count)
+            let polyline1 = MKGeodesicPolyline(coordinates: coordinates1, count: coordinates1.count)
+            let polyline2 = MKGeodesicPolyline(coordinates: coordinates2, count: coordinates2.count)
+            let polyline3 = MKGeodesicPolyline(coordinates: coordinates3, count: coordinates3.count)
+            let polyline4 = MKGeodesicPolyline(coordinates: coordinates4, count: coordinates4.count)
+            let polyline5 = MKGeodesicPolyline(coordinates: coordinates5, count: coordinates5.count)
+            let polyline6 = MKGeodesicPolyline(coordinates: coordinates6, count: coordinates6.count)
+            let polyline7 = MKGeodesicPolyline(coordinates: coordinates7, count: coordinates7.count)
+            uiView.addOverlay(polyline)
+            uiView.addOverlay(polyline0)
+            uiView.addOverlay(polyline1)
+            uiView.addOverlay(polyline2)
+            uiView.addOverlay(polyline3)
+            uiView.addOverlay(polyline4)
+            uiView.addOverlay(polyline5)
+            uiView.addOverlay(polyline6)
+            uiView.addOverlay(polyline7)
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, MKMapViewDelegate, UIGestureRecognizerDelegate {
+        var parent: MapView
+        var tapGestureRecognizer = UITapGestureRecognizer()
+        var longPressGestureRecognizer = UILongPressGestureRecognizer()
+        init(_ parent: MapView) {
+            self.parent = parent
+            super.init()
+            self.tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
+            self.tapGestureRecognizer.delegate = self
+            self.parent.mapView.addGestureRecognizer(tapGestureRecognizer)
+            self.longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(longPressHandler))
+            self.longPressGestureRecognizer.delegate = self
+            self.longPressGestureRecognizer.minimumPressDuration = 1.0
+            self.parent.mapView.addGestureRecognizer(longPressGestureRecognizer)
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            if let polyline = overlay as? MKPolyline {
+                let polylineRenderer = MKPolylineRenderer(polyline: polyline)
+                polylineRenderer.strokeColor = .red
+                polylineRenderer.lineWidth = 3.0
+                return polylineRenderer
+            }
+            return MKOverlayRenderer()
+        }
+        
+        @objc func longPressHandler(_ gesture: UILongPressGestureRecognizer) {
+            let location = longPressGestureRecognizer.location(in: self.parent.mapView)
+            let coordinate = self.parent.mapView.convert(location, toCoordinateFrom: self.parent.mapView)
+            self.parent.coordinate = coordinate
+            
+            if gesture.state == UILongPressGestureRecognizer.State.ended {
+
+            } else if gesture.state == UILongPressGestureRecognizer.State.began {
+                print("\(coordinate.latitude), \(coordinate.longitude)")
+                self.parent.previousCheckPoints = self.parent.checkPoints
+                let checkPoint = CheckPoint(
+                    title: "Hi",
+                    coordinate: .init(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                )
+                self.parent.checkPoints = [checkPoint]
+                
+                let distance = 100000000.0
+                
+                self.parent.coordinates = []
+                self.parent.coordinates.append(coordinate)
+                let l0 = self.parent.getLocation(origin: coordinate, angle: 15.0, distance: distance)
+                print("0: \(l0.latitude), \(l0.longitude)")
+                self.parent.coordinates.append(l0)
+                
+                self.parent.coordinates0 = []
+                self.parent.coordinates0.append(coordinate)
+                let l00 = self.parent.getLocation(origin: coordinate, angle: 75.0, distance: distance)
+                print("0: \(l00.latitude), \(l00.longitude)")
+                self.parent.coordinates0.append(l00)
+                
+                self.parent.coordinates1 = []
+                self.parent.coordinates1.append(coordinate)
+                let l1 = self.parent.getLocation(origin: coordinate, angle: 105.0, distance: distance)
+                print("1: \(l1.latitude), \(l1.longitude)")
+                self.parent.coordinates1.append(l1)
+                
+                self.parent.coordinates2 = []
+                self.parent.coordinates2.append(coordinate)
+                let l2 = self.parent.getLocation(origin: coordinate, angle: 165.0, distance: distance)
+                print("2: \(l2.latitude), \(l2.longitude)")
+                self.parent.coordinates2.append(l2)
+                
+                self.parent.coordinates3 = []
+                self.parent.coordinates3.append(coordinate)
+                let l3 = self.parent.getLocation(origin: coordinate, angle: 195.0, distance: distance)
+                print("3: \(l3.latitude), \(l3.longitude)")
+                self.parent.coordinates3.append(l3)
+                
+                self.parent.coordinates4 = []
+                self.parent.coordinates4.append(coordinate)
+                let l4 = self.parent.getLocation(origin: coordinate, angle: 255.0, distance: distance)
+                print("4: \(l4.latitude), \(l4.longitude)")
+                self.parent.coordinates4.append(l4)
+                
+                self.parent.coordinates5 = []
+                self.parent.coordinates5.append(coordinate)
+                let l5 = self.parent.getLocation(origin: coordinate, angle: 285.0, distance: distance)
+                print("5: \(l5.latitude), \(l5.longitude)")
+                self.parent.coordinates5.append(l5)
+                
+                self.parent.coordinates6 = []
+                self.parent.coordinates6.append(coordinate)
+                let l6 = self.parent.getLocation(origin: coordinate, angle: 345.0, distance: distance)
+                print("6: \(l6.latitude), \(l6.longitude)")
+                self.parent.coordinates6.append(l6)
+                
+                self.parent.coordinates7 = []
+                self.parent.coordinates7.append(coordinate)
+                let l7 = self.parent.getLocation(origin: coordinate, angle: 0.0, distance: distance)
+                print("7: \(l7.latitude), \(l7.longitude)")
+                self.parent.coordinates7.append(l7)
+            }
+        }
+        
+        @objc func tapHandler(_ gesture: UITapGestureRecognizer) {
+        }
+    }
+    
+    func getLocation(origin: CLLocationCoordinate2D, angle: Double, distance: Double) -> CLLocationCoordinate2D {
+        let distanceFraction = distance / 6371000.0
+        let angleRadian = angle * Double.pi / 180.0
+        
+        print("input: \(origin.latitude), \(origin.longitude)")
+
+        let latitudeSourceRadian = origin.latitude * Double.pi / 180.0
+        let longitudeSourceRadian = origin.longitude * Double.pi / 180.0
+
+        let latitudeDestinationRadian = asin(sin(latitudeSourceRadian) * cos(distanceFraction) + cos(latitudeSourceRadian) * sin(distanceFraction) * cos(angleRadian))
+        let longitudeDestinationRadianTemp =  atan2(sin(angleRadian) * sin(distanceFraction) * cos(latitudeSourceRadian), cos(distanceFraction) - sin(latitudeSourceRadian) * sin(latitudeDestinationRadian))
+        let longitudeDestinationRadianTemp2 = (longitudeSourceRadian + longitudeDestinationRadianTemp + 3 * Double.pi)
+        let longitudeDestinationRadianTemp3 = longitudeDestinationRadianTemp2.truncatingRemainder(dividingBy: (2 * Double.pi))
+        let longitudeDestinationRadian = longitudeDestinationRadianTemp3 - Double.pi
+        
+        let latitudeDestinationDegree = latitudeDestinationRadian * 180.0 / Double.pi
+        let longitudeDestinationDegree = longitudeDestinationRadian * 180.0 / Double.pi
+        
+        print("output: \(latitudeDestinationDegree), \(longitudeDestinationDegree)")
+        return CLLocationCoordinate2DMake(latitudeDestinationDegree, longitudeDestinationDegree)
+    }
+
+}
+
+

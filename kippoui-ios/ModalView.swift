6a60071dfa8b00c06a2dcb286e85797d433ec42d
kinakoankon <kinakoankon@gmail.com>
2020-09-13 23:13:13 +0900

    設定保存ボタン

--- kippoui-ios/ModalView.swift
+++ kippoui-ios/ModalView.swift
@@ -1,5 +1,9 @@
 import SwiftUI
 
+enum ActiveAlert {
+    case error, save, none
+}
+
 struct ModalView: View {
     
     @EnvironmentObject var preferences: Preferences
@@ -7,6 +11,9 @@ struct ModalView: View {
     
     @State var updatedArgument = ""
     var angle = ["30", "45"]
+    
+    @State var isAlert = false;
+    @State var activeAlert: ActiveAlert = .error
     
     let formatter: NumberFormatter = {
         let numFormatter = NumberFormatter()
@@ -22,7 +29,7 @@ struct ModalView: View {
                         if editingChanged {
                             
                         } else {
-
+                            
                         }
                     }).onChange(of: preferences.argument) {text in
                         updatedArgument = text
@@ -52,25 +59,90 @@ struct ModalView: View {
                 }
             }
             .navigationTitle("Preferences")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    print("updatedArgument = \(updatedArgument), preferences.selected = \(preferences.selected), preferences.angle = \(self.preferences.angle)")
+                    if updatedArgument == "" && preferences.selected == Int(self.preferences.angle) {
+                        self.isAlert = true
+                        self.activeAlert = .none
+                        return
+                    }
+                    if updatedArgument == preferences.argument && preferences.selected == Int(self.preferences.angle) {
+                        self.isAlert = true
+                        self.activeAlert = .none
+                        return
+                    }
+                    if updatedArgument == "" && preferences.selected != Int(self.preferences.angle) {
+                        switch preferences.selected {
+                        case 0:
+                            self.preferences.angle = "0"
+                        case 1:
+                            self.preferences.angle = "1"
+                        default:
+                            self.preferences.angle = "0"
+                        }
+                        self.isAlert = true
+                        self.activeAlert = .save
+                        return
+                    } else {
+                        do {
+                            let regex = try NSRegularExpression(pattern: "^([1-9]\\d*|0)(\\.\\d+)?$", options: [])
+                            let results = regex.matches(in: updatedArgument, options: [], range: NSRange(0..<updatedArgument.count))
+                            if results.count == 0 {
+                                self.isAlert = true
+                                self.activeAlert = .error
+                            } else {
+                                if updatedArgument != "" {
+                                    self.preferences.argument = updatedArgument
+                                }
+                                switch preferences.selected {
+                                case 0:
+                                    self.preferences.angle = "0"
+                                case 1:
+                                    self.preferences.angle = "1"
+                                default:
+                                    self.preferences.angle = "0"
+                                }
+                                self.isAlert = true
+                                self.activeAlert = .save
+                            }
+                        } catch {
+                            print("\(error)")
+                        }
+                    }
+                }) {
+                    Text("Save")
+                }
+                .alert(isPresented: $isAlert) {
+                    switch activeAlert {
+                    case .error:
+                        return Alert(title: Text("エラー"), message: Text("偏角を正しく入力してください。"), dismissButton: .default(Text("わかった")))
+                    case .save:
+                        return Alert(title: Text("成功"), message: Text("設定が保存されました。"), dismissButton: .default(Text("わかった")))
+                    case .none:
+                        return Alert(title: Text("エラー"), message: Text("何も変更されていません。"), dismissButton: .default(Text("わかった")))
+                    }
+                }
+            )
         }.onAppear(perform: {
             //print("\(#file) - \(#function)")
         })
         .onDisappear(perform: {
-            if updatedArgument != "" {
-                self.preferences.argument = updatedArgument
-            }
-            //print("selected = \(preferences.selected)")
-            switch preferences.selected {
-            case 0:
-                self.preferences.angle = "0"
-            case 1:
-                self.preferences.angle = "1"
-            default:
-                self.preferences.angle = "0"
-            }
+//            if updatedArgument != "" {
+//                self.preferences.argument = updatedArgument
+//            }
+//            //print("selected = \(preferences.selected)")
+//            switch preferences.selected {
+//            case 0:
+//                self.preferences.angle = "0"
+//            case 1:
+//                self.preferences.angle = "1"
+//            default:
+//                self.preferences.angle = "0"
+//            }
         })
 //        .onTapGesture {
-//            UIApplication.shared.endEditing()
+//            UIApplication.shared.endEditing() // Picker機能しない
 //        }
     }
 }
